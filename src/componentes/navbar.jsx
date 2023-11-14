08ebf8918a0196c7f78c945c3dd73032406ea32a
Your Name <you@example.com>
2023-11-14 00:35:15 -0300

    cambios

--- src/componentes/navbar.jsx
+++ src/componentes/navbar.jsx
@@ -9,26 +9,27 @@ const Navbar = () => {
           <img className="logo-img" src={logo} alt="" />
         </a>
         <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+  className="navbar-toggler"
+  type="button"
+  data-bs-toggle="collapse"
+  data-bs-target="#navbarNavAltMarkup"
+  aria-controls="navbarNavAltMarkup"
+  aria-expanded="false"
+  aria-label="Toggle navigation"
+  style={{ marginLeft: 'auto' }} // Agrega esta línea
+>
+  <span className="navbar-toggler-icon"></span>
+</button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav ms-auto "style={{ marginRight: '90px' }}>
-            <a className="nav-link active" aria-current="page" href="/">
-              Portafolio
-            </a>
-            <a className="nav-link active" href="/sobremi">
-              Sobre mí
-            </a>
-          </div>
-        </div>
+  <div className="navbar-nav ms-auto" style={{ marginLeft: 'auto' }}>
+    <a className="nav-link active" aria-current="page" href="/">
+      Portafolio
+    </a>
+    <a className="nav-link active" href="/sobremi">
+      Sobre mí
+    </a>
+  </div>
+</div>
       </div>
     </nav>
   );
